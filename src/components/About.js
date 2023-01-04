ebca903ef4b10920b4f16e3a1d2e5da45d9290a1
Rodrigo Manica <rodrigomanica5@gmail.com>
2023-01-04 16:57:48 -0300

    Bio & Featured Work details

--- src/components/About.js
+++ src/components/About.js
@@ -11,11 +11,12 @@ function About() {
     <>
       <div className='aboutMe' ref={about}>
         <div className='detailContainer'>
-          <h2 className='sectionTitle'>about me</h2>
+          <h2 className='sectionTitle'>hello you</h2>
           <div className='aboutInfo'>
-            <p>Welcome to my portfolio!</p>
-            <p>I'm a React.js Frontend Developer currently based in Germany, and at the moment I'm working as Teaching Assistant of JavaScript & React.js courses at Coderhouse</p>
-            <p>Since many years I have been passionate about the software development industry, and enthusiastic on learning new skills and tools.</p>
+            <p className='aboutInfoSpan'>My name is Rodrigo and I'm a React.js Frontend Developer currently based in Europe.</p>
+            <p>Since I was a teenager I found myself very interested in everything related to computers and programming. I still remember when I was in school, I couldn't wait to go back home, turn my computer on, and start developing small softwares in Visual Basic 6 just for fun! Fixing computers and being curious about Cybersecurity were also some of the things that kept me amused in my free time.</p>
+            <p>Later I decided to study International Business in Argentina, my home country, while doing an exchange semester in Belgium to improve my English skills but mainly because I have always been very motivated in meeting people from different cultures and open myself up to what the World had to offer.</p>
+            <p>And even though it was an amazing part of my life, having completed my studies, achieving a bachelor degree in International Business, and working in a marketing company, I felt that I had to pursue my life vocation of becoming a Web Developer. And I did it, because one of the things that defines me the most is that I pursue my dreams and goals no matter how much time and effort they take.</p>
           </div>
           <a className='btn' href='https://drive.google.com/file/d/1RLGbbMTLw94sveqZhqj8aevARKU5QLjE/view' target="_blank">MY RESUME</a>
         </div>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,15 +4,15 @@ function Skills() {
     return (
         <div className='skillsContainer'>
             <div>
-                <img src='/assets/images/react-js.svg' alt='React.js' width={200}></img>
+                <img src='/assets/images/react-js.svg' alt='React.js' width={180}></img>
                 <p></p>
             </div>
             <div>
-                <img src='/assets/images/react-native.svg' alt='React Native' width={200}></img>
+                <img src='/assets/images/react-native.svg' alt='React Native' width={180}></img>
                 <p></p>
             </div>
             <div>
-                <img src='/assets/images/redux.svg' alt='Redux' width={200}></img>
+                <img src='/assets/images/redux.svg' alt='Redux' width={180}></img>
                 <p></p>
             </div>
         </div>
